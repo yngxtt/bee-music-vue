546eea8776412ad24a963015b57df5d88f0ce4bc
oldBe <yngxtt@163.com>
2022-04-21 02:29:05 +0800

    打包配置

--- vue.config.js
+++ vue.config.js
@@ -8,8 +8,8 @@ module.exports = {
         proxy: {
             "^/beeapi": {
               target: "http://localhost:3000",
-              ws: true, //代理websockets
-              changeOrigin: true, //开启代理：在本地会创建一个虚拟服务端，然后发送请求的数据，并同时接收请求							的数据，这样服务端和服务端进行数据的交互就不会有跨域问题
+              ws: true, //代理 websockets
+              changeOrigin: true, //开启代理: 在本地会创建一个虚拟服务端，然后发送请求的数据，并同时接收请求							的数据，这样服务端和服务端进行数据的交互就不会有跨域问题
               pathRewrite: {  //重写匹配的字段，如果不需要在请求路径上，重写为""
                   "^/beeapi": ""
               }
